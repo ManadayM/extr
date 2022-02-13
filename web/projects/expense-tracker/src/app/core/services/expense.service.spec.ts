073a2295ae5c9bd0181049979ff4758b15e28446
Rajendrasinh Parmar <rajendra20791@gmail.com>
2022-02-14 01:15:57 +0530

    test(web): Implement unit tests for expense service

--- web/projects/expense-tracker/src/app/core/services/expense.service.spec.ts
+++ web/projects/expense-tracker/src/app/core/services/expense.service.spec.ts
@@ -1,21 +1,50 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { ExpenseService } from './expense.service';
 
 describe('ExpenseService', () => {
   let service: ExpenseService;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: []
+      providers: [],
     });
 
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
     service = TestBed.inject(ExpenseService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  describe('getExpenses', () => {
+    it('should get expenses list', () => {
+      const testData = [
+        {
+          amount: 10,
+          name: 'Fruits',
+          expenseDate: '2022-01-28T18:30:00.000Z',
+        },
+      ];
+
+      service.getExpenses().subscribe((res) => {
+        expect(res).toBeTruthy();
+      });
+
+      const req = httpTestingController.expectOne('http://localhost:3000/expenses');
+
+      expect(req.request.method).toEqual('GET');
+
+      req.flush(testData);
+
+      httpTestingController.verify();
+    });
   });
 });
