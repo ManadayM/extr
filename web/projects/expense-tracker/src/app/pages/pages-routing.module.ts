d5d949dec7a31be83e2b6b67f1e5ae7ae9fb37d3
Manaday Mavani <manadaym@users.noreply.github.com>
2022-02-13 16:50:33 +0530

    add(web-pages): chore

--- web/projects/expense-tracker/src/app/pages/pages-routing.module.ts
+++ web/projects/expense-tracker/src/app/pages/pages-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ExpensesComponent } from './expenses/expenses.component';
+
 import { PagesComponent } from './pages.component';
 
 const routes: Routes = [
@@ -8,8 +8,8 @@ const routes: Routes = [
     path: '',
     component: PagesComponent,
     children: [
-      { path: 'expenses/:id', loadChildren: () => import('./expense-detail/expense-detail.module').then(m => m.ExpenseDetailModule) },
       { path: 'expenses', loadChildren: () => import('./expenses/expenses.module').then(m => m.ExpensesModule) },
+      { path: 'expenses/:id', loadChildren: () => import('./expense-detail/expense-detail.module').then(m => m.ExpenseDetailModule) },
       { path: '', redirectTo: 'expenses', pathMatch: 'full' },
       { path: '**', redirectTo: 'expenses' }
     ]
