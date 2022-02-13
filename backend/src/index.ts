009706dfbce1cb5c99d2fd9bc36cf709ce608385
Rajendrasinh Parmar <rajendra20791@gmail.com>
2022-02-14 01:33:47 +0530

    add(backend): Implement graceful shutdown

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,14 +4,17 @@ import app from "./app";
 
 import { DBConfig, ServerConfig } from "./types";
 import { logger } from "./services";
+import { Server } from "http";
 
-const dbConfig: DBConfig = config.get('db');
-const serverConfig: ServerConfig = config.get('server');
+const dbConfig: DBConfig = config.get("db");
+const serverConfig: ServerConfig = config.get("server");
+
+let server: Server;
 
 // Start listening only when you've made to the database! ¯\_(ツ)_/¯
-app.on('dbConnected', () => {
+app.on("dbConnected", () => {
   const PORT = serverConfig.port || 8080;
-  app.listen(PORT, () => logger.info(`App is listening on port ${PORT}.`));
+  server = app.listen(PORT, () => logger.info(`App is listening on port ${PORT}.`));
 });
 
 db.connect({
@@ -19,10 +22,33 @@ db.connect({
   host: dbConfig.host,
   port: dbConfig.port,
   user: dbConfig.user,
-  password: dbConfig.password
+  password: dbConfig.password,
 })
   .then(() => {
-    logger.info('Database connected.')
-    app.emit('dbConnected')
+    logger.info("Database connected.");
+    app.emit("dbConnected");
   })
   .catch((err) => console.error(err));
+
+function gracefulShutdown() {
+  logger.info("Closing http server.");
+  server.close(() => {
+    logger.info("Http server closed.");
+
+    db.close().then(() => {
+      logger.info("database connection closed.");
+      logger.info("Bye bye!");
+      process.exit(0);
+    });
+  });
+}
+// process.stdin.resume();
+process.on("SIGINT", () => {
+  logger.info("SIGINT signal received.");
+  gracefulShutdown();
+});
+
+process.on("SIGTERM", () => {
+  logger.info("SIGTERM signal received.");
+  gracefulShutdown();
+});
